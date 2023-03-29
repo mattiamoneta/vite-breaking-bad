ac5cdd2bc327ebd00e906bf645ac83b2f9d16b2f
mattiamoneta <in.mattiamoneta@gmail.com>
2023-03-29 15:31:01 +0200

    Init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://ygoprodeck.com/cdn-cgi/image/width=140/https://images.ygoprodeck.com/images/assets/ygoprodeck_header_logo.png?v=1.13"
+                    alt="Logo" class="d-inline-block align-text-top me-3">
+                {{ title }}
+            </a>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+export default {
+    name: 'AppHeader',
+    props: {
+        title: String
+    }
+}
+
+</script>
+
+
+<style></style>
